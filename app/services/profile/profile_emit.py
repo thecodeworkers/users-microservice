f3044bc5b1a94059bb0539944884dcddf59b6aaf
Victor Caceres <caceres.vc95@gmail.com>
2021-01-18 15:09:26 -0400

    Create get profile channel

--- app/services/profile/profile_emit.py
+++ app/services/profile/profile_emit.py
@@ -9,21 +9,37 @@ class ProfileEmitter():
     def emit_create_profile(self):
         service_bus_connection.add_queue('create_profile', self.__create_profile)
 
+    def emit_get_profile(self):
+        service_bus_connection.add_queue('get_profile', self.__get_profile)
+
     def __create_profile(self, data):
         try:
             profile = Profiles(**data).save()
-            profile = parser_one_object(profile)
+            return self.__profile_instance(profile)
 
-            del profile['user']
-            del profile['id']
+        except Exception as error:
+            return str(error)
 
-            return profile
+    def __get_profile(self, data):
+        try:
+            profile = Profiles.objects(user=data['id']).first()
+            if not profile: return {}
+
+            return self.__profile_instance(profile)
 
         except Exception as error:
             return str(error)
 
     def __start_emitters(self):
         self.emit_create_profile()
+        self.emit_get_profile()
+
+    def __profile_instance(self, profile):
+        profile = parser_one_object(profile)
+        del profile['user']
+        del profile['id']
+
+        return profile
 
 def start_profile_emit():
     ProfileEmitter()
